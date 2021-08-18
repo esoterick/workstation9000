89e30e111adbda4ec38a8c516b9478eebb275eb8
Robert J. Lambert III <robert@digitalocean.com>
2021-08-18 12:09:54 -0400

    add custom fish user key bindings

--- nix/homes/common.nix
+++ nix/homes/common.nix
@@ -23,8 +23,10 @@
 
   home.file.".gitconfig".source = ../../dotfiles/gitconfig;
 
-  home.file.".config/fish/config.fish".source = ../../dotfiles/fish/config.fish;
   home.file.".config/starship.toml".source = ../../dotfiles/starship.toml;
+
+  home.file.".config/fish/config.fish".source = ../../dotfiles/fish/config.fish;
+  home.file.".config/fish/functions/fish_user_key_bindings.fish".source = ../../dotfiles/fish/functions/fish_user_key_bindings.fish;
 
   home.file.".doom.d/init.el".source = ../../dotfiles/doom.d/init.el;
   home.file.".doom.d/config.el".source = ../../dotfiles/doom.d/init.el;
