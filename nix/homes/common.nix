456c43873845b67ae4aadbd6969ad048ac0d5c81
Robert J. Lambert III <robert@digitalocean.com>
2021-08-18 12:26:09 -0400

    added doom emacs, my doom config, and start service

--- nix/homes/common.nix
+++ nix/homes/common.nix
@@ -1,20 +1,11 @@
 { config, pkgs, ... }:
-
-# let
-#   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
-#     url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
-#   }) {
-#     doomPrivateDir = ../../dotfiles/doom.d;
-#   };
-# in {
-# {
-#   home.packages = [ doom-emacs ];
-#  home.file.".emacs.d/init.el".text = ''
-#       (load "default.el")
-#  '';
-
-
-{
+let
+  doom-emacs = pkgs.callPackage (builtins.fetchTarball {
+    url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
+  }) {
+    doomPrivateDir = ../../dotfiles/doom.d;
+  };
+in {
   programs.home-manager.enable = true;
   home.username = "rlambert";
   home.homeDirectory = "/home/rlambert";
@@ -31,4 +22,14 @@
   home.file.".doom.d/init.el".source = ../../dotfiles/doom.d/init.el;
   home.file.".doom.d/config.el".source = ../../dotfiles/doom.d/init.el;
   home.file.".doom.d/packages.el".source = ../../dotfiles/doom.d/packages.el;
+
+  home.packages = [ doom-emacs ];
+  home.file.".emacs.d/init.el".text = ''
+      (load "default.el")
+  '';
+
+  services.emacs = {
+    enable = true;
+    package = doom-emacs;
+  };
 }
