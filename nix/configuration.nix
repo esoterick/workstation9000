436d3471b04d6b9446fb497021c6f18e5a45e15f
Robert J. Lambert III <robert@digitalocean.com>
2021-11-19 08:12:02 -0500

    spacing

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -16,7 +16,4 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE0y63gYBa2F+lBNISfNvCsDQPeHNyPIFgWWji2kC3hO rl@Roberts-MBP.hyrule.local"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhT5xDOtpRXYZQlebper6c0ZwC3JvTEOAcXb7j2+bp5 rl@rl-mbp"
   ];
-}  
-
-
-
+}
