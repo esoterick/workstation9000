f44a201b370aaa95649af049167d7cef1188d15b
Robert J. Lambert III <robert@digitalocean.com>
2021-11-19 08:12:18 -0500

    no ipv6

--- nix/networking.nix
+++ nix/networking.nix
@@ -6,7 +6,6 @@
  "10.74.61.46"
  ];
     defaultGateway = "10.124.56.1";
-    defaultGateway6 = "";
     dhcpcd.enable = false;
     usePredictableInterfaceNames = lib.mkForce false;
     interfaces = {
@@ -14,11 +13,7 @@
         ipv4.addresses = [
           { address="10.124.57.31"; prefixLength=21; }
         ];
-        ipv6.addresses = [
-          { address="fe80::ecce:40ff:feb0:79fc"; prefixLength=64; }
-        ];
         ipv4.routes = [ { address = "10.124.56.1"; prefixLength = 32; } ];
-        ipv6.routes = [ { address = ""; prefixLength = 128; } ];
       };
       
     };
